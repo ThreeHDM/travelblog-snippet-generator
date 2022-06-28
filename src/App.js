76c77fd1f1f159f5b57043ffddabeec03267559f
ThreeHDM <juanbilardi@gmail.com>
2022-06-28 12:16:42 -0300

    created ul list tour generator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [formState, setFormState] = useState({
+    precio: "",
+    duracion: "",
+    info: "",
+    traslados: "",
+    salidas: "",
+  });
+
+  const handleChange = (event) => {
+    setFormState((previousProps) => ({
+      ...previousProps,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const textAreaValue = `<ul class="no-bullets">
+    <li><i class="fa fa-usd" aria-hidden="true" style="margin-left: 4px;"></i> <b>Precio:</b> ${formState.precio}</li>
+    <li><i class="fa fa-clock-o" aria-hidden="true"></i> <b>Duración:</b> ${formState.duracion}</li>
+    <li><i class="fa fa-info-circle" aria-hidden="true"></i> <b>Info:</b> ${formState.info}</li>
+    <li><i class="fas fa-bus"></i> <b>Traslados:</b>  ${formState.traslados}</li>
+    <li><i class="fa fa-calendar" aria-hidden="true"></i> <b>Salidas:</b>  ${formState.salidas}</li>
+</ul>`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <form className="box">
+          <label>Precio </label>
+          <input
+            type="text"
+            name="precio"
+            value={formState.precio}
+            onChange={handleChange}
+          />
+          <label>Duración </label>
+          <input
+            type="text"
+            name="duracion"
+            value={formState.duracion}
+            onChange={handleChange}
+          />
+          <label>Info </label>
+          <textarea
+            name="info"
+            value={formState.info}
+            onChange={handleChange}
+          />
+          <label>Traslados </label>
+          <input
+            type="text"
+            name="traslados"
+            value={formState.traslados}
+            onChange={handleChange}
+          />
+          <label>Salidas </label>
+          <input
+            type="text"
+            name="salidas"
+            value={formState.salidas}
+            onChange={handleChange}
+          />
+        </form>
+
+        <div className="box">
+          <textarea
+            defaultValue="aquí se genera el snippet"
+            value={textAreaValue}
+            rows="10"
+            readonly
+          />
+        </div>
       </header>
     </div>
   );
